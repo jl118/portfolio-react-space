f812ccccc77e12f142638043f5f37760047a7616
jl118 <jen.liebelt@gmail.com>
2022-02-23 17:40:12 -0500

    changed browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 // import reportWebVitals from './reportWebVitals';
 import '../node_modules/normalize.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
